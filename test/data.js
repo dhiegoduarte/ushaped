c3243eb6e3d07cf777a3d53c129f7573faec493c
Dhiego Duarte <dhiego.duarte@terceiros.net.com.br>
2018-05-10 15:50:59 -0300

    corrigido /test/data.js

--- test/data.js
+++ test/data.js
@@ -7,9 +7,27 @@ var contatos = [{nome: "xyz1", email: 'xyz1@email.com.br'},
 {nome: "xyz3", email: 'xyz3@email.com.br'},
 ];
 
-MongoClient.connect('mongodb://127.0.0.1:27017/ushaped_test',
-	function(erro, db) {
+// MongoClient.connect('mongodb://127.0.0.1:27017/ushaped_test',
+// 	function(erro, db) {
+// 		if(erro) throw err;
+// 		db.dropDatabase(function(err) {
+// 			if(err) return console.log(err);
+// 			console.log('Banco apagado com sucesso')
+// 			db.collection('contatos').insert(contatos,
+// 				function(err, inserted) {
+// 					if(err) return console.log(err);
+// 					console.log('Banco populado com sucesso')
+// 					process.exit(0);
+// 				});
+// 		});
+// 	}
+// 	);
+
+//Alterado devido a versão 3.0.0 ou maior do mongo
+MongoClient.connect('mongodb://127.0.0.1:27017',
+	function(erro, client) {
 		if(erro) throw err;
+		var db = client.db('ushaped_test');
 		db.dropDatabase(function(err) {
 			if(err) return console.log(err);
 			console.log('Banco apagado com sucesso')
